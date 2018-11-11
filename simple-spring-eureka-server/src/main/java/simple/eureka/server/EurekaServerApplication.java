e9a5d76ce3ff3071bb7b653ff47b9ed614d41693
us <us@DESKTOP-PCPONUL>
2018-11-11 21:41:30 +0900

    config/eureka/apigateway/service 연동구현

---
+++ simple-spring-eureka-server/src/main/java/simple/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,31 @@
+package simple.eureka.server;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
