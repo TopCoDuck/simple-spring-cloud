6a78ebf62b4e70b28af5c1ca89fb96ba4848a2f9
us <us@DESKTOP-PCPONUL>
2018-11-11 21:44:32 +0900

    config/eureka/apigateway/service 연동구현

---
+++ simple-apigateway/src/main/java/simple/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package simple.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+}
