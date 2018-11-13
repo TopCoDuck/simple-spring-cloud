0993cbb76e74ecef69efc54abc1b18caf47c4ddb
us <us@DESKTOP-PCPONUL>
2018-11-13 20:27:41 +0900

    동기요청 추가

--- simple-item-service/src/main/java/simple/item/controller/ItemController.java
+++ simple-item-service/src/main/java/simple/item/controller/ItemController.java
@@ -1,6 +1,8 @@
 package simple.item.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import simple.item.model.Item;
@@ -10,7 +12,8 @@ import simple.item.model.ItemState;
 public class ItemController {
 	
 	@GetMapping("/items/{itemId}")
-	public Item getItem(String itemId) {
+	public Item getItem(@PathVariable String itemId) {
+		System.out.println("ItemController");
 		Item item = new Item();
 		item.setItemId("ITEM_ID");
 		item.setItemNm("ITEM_NM");
--- simple-review-service/src/main/java/simple/review/controller/ReviewController.java
+++ simple-review-service/src/main/java/simple/review/controller/ReviewController.java
@@ -1,30 +1,34 @@
 package simple.review.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import simple.review.model.Item;
+import simple.review.model.ItemState;
 import simple.review.model.Review;
 
 
 @RestController
 public class ReviewController {
 	
-	@GetMapping("/")
-	public String getTest() {
-		return "test";
-	}
+	@Autowired
+	RestTemplate restTemplate;
 	
-	@GetMapping("/{test}")
-	public String getTest2() {
-		return "test2";
-	}
 	@GetMapping("/reviews/{reviewSeq}")
 	public Review getReview(@PathVariable int reviewSeq) throws Exception{
+		Item item = restTemplate.getForObject("http://gateway-1/items/1", Item.class);
+
 		Review review = new Review();
-		review.setReiwSeq(1);
-		review.setItemId("ITEM_ID");
-		review.setContent("내용");
+		if(item.getItemState() == ItemState.LIVE) {
+			review.setReiwSeq(1);
+			review.setItemId(item.getItemId());
+			review.setContent("내용");
+		}
+
 		return review;
+		
 	}
 }
